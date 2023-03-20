4b5c6f4e67ece502d4a68b7a54bce45e85a42103
jwadolowski <6334715+jwadolowski@users.noreply.github.com>
2023-03-20 18:32:49 +0100

    feat: AstroNvim customziations (initial commit)

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,17 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+
+  -- colorscheme
+  { import = "astrocommunity.colorscheme.tokyonight" },
+
+  -- pack
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.typescript" },
 }
